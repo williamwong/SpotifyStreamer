8735f0c83b4d0cbf04a7f73b560c65fa74adcf37
William Wong <williamwong09@gmail.com>
2015-11-30 00:56:12 -0500

    Bind SeekBar to view model

--- app/src/main/java/org/williamwong/spotifystreamer/fragments/PlayerFragment.java
+++ app/src/main/java/org/williamwong/spotifystreamer/fragments/PlayerFragment.java
@@ -2,19 +2,15 @@ package org.williamwong.spotifystreamer.fragments;
 
 import android.app.Dialog;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.SeekBar;
 
 import org.williamwong.spotifystreamer.R;
 import org.williamwong.spotifystreamer.databinding.FragmentPlayerBinding;
-import org.williamwong.spotifystreamer.services.MusicService;
-import org.williamwong.spotifystreamer.utilities.TimerUtilities;
 import org.williamwong.spotifystreamer.viewModels.PlayerViewModel;
 
 /**
@@ -23,78 +19,15 @@ import org.williamwong.spotifystreamer.viewModels.PlayerViewModel;
  */
 public class PlayerFragment extends DialogFragment {
 
-    private MusicService mMusicService;
-
-    private SeekBar mSeekBar;
-    private Handler mHandler = new Handler();
-    private Runnable mUpdateSeekBar;
-
-    private FragmentPlayerBinding mBinding;
-    private PlayerViewModel mViewModel;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_player, container, false);
-        mBinding = FragmentPlayerBinding.bind(view);
-        mViewModel = new PlayerViewModel();
-        mBinding.setVm(mViewModel);
 
-        mSeekBar = (SeekBar) view.findViewById(R.id.previewSeekBar);
-
-        mMusicService = MusicService.getMusicService();
-        if (mMusicService != null) {
-            mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-                }
-
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-                    // Remove message Handler from updating progress bar
-                    mHandler.removeCallbacks(mUpdateSeekBar);
-                }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-                    mHandler.removeCallbacks(mUpdateSeekBar);
-                    int totalDuration = (int) mMusicService.getDuration();
-                    int currentPosition = TimerUtilities.progressToTimer(seekBar.getProgress(), totalDuration);
-
-                    // forward or backward to certain seconds
-                    mMusicService.seekTo(currentPosition);
-
-                    // update timer progress again
-                    updateSeekBar();
-                }
-            });
-            mUpdateSeekBar = new Runnable() {
-                @Override
-                public void run() {
-                    if (mMusicService.isPlayingOrPreparing()) {
-                        long currentDuration = mMusicService.getCurrentPosition();
-                        long totalDuration = mMusicService.getDuration();
-
-                        mViewModel.timeCurrent.set(TimerUtilities.milliSecondsToTimer(currentDuration));
-                        mViewModel.timeEnd.set(TimerUtilities.milliSecondsToTimer(totalDuration));
-
-                        int progress = TimerUtilities.getProgressPercentage(currentDuration, totalDuration);
-                        mSeekBar.setProgress(progress);
-                    }
-
-                    mHandler.postDelayed(this, 100);
-                }
-            };
-        }
-
-        updateSeekBar();
-        mViewModel.setTrack(mMusicService.getCurrentlyPlayingTrackModel());
+        FragmentPlayerBinding binding = FragmentPlayerBinding.bind(view);
+        PlayerViewModel viewModel = new PlayerViewModel();
+        binding.setVm(viewModel);
 
         return view;
-    }
-
-    private void updateSeekBar() {
-        mHandler.postDelayed(mUpdateSeekBar, 100);
     }
 
     @Override
--- app/src/main/java/org/williamwong/spotifystreamer/utilities/TimerUtilities.java
+++ app/src/main/java/org/williamwong/spotifystreamer/utilities/TimerUtilities.java
@@ -12,7 +12,7 @@ public class TimerUtilities {
      * @param milliseconds Number of milliseconds to convert to timer string
      * @return String representation of time in the format of hours:minutes:seconds
      */
-    public static String milliSecondsToTimer(long milliseconds) {
+    public static String milliSecondsToTimeString(long milliseconds) {
         String finalTimerString = "";
         String secondsString;
 
@@ -41,20 +41,12 @@ public class TimerUtilities {
     /**
      * Gets progress percentage of track played
      *
-     * @param currentDuration Current point in time of playing track in milliseconds
+     * @param currentPosition Current point in time of playing track in milliseconds
      * @param totalDuration   Total length of track in milliseconds
      * @return Percentage of track played as integer
      */
-    public static int getProgressPercentage(long currentDuration, long totalDuration) {
-        Double percentage;
-
-        long currentSeconds = (int) (currentDuration / 1000);
-        long totalSeconds = (int) (totalDuration / 1000);
-
-        // calculating percentage
-        percentage = (((double) currentSeconds) / totalSeconds) * 100;
-
-        // return percentage
+    public static int millisecondsToPercentage(long currentPosition, long totalDuration) {
+        Double percentage = (((double) currentPosition / totalDuration) * 100);
         return percentage.intValue();
     }
 
@@ -65,12 +57,7 @@ public class TimerUtilities {
      * @param totalDuration Total duration of current track in milliseconds
      * @return Current duration in milliseconds
      */
-    public static int progressToTimer(int progress, int totalDuration) {
-        int currentDuration;
-        totalDuration = totalDuration / 1000;
-        currentDuration = (int) ((((double) progress) / 100) * totalDuration);
-
-        // return current duration in milliseconds
-        return currentDuration * 1000;
+    public static int percentageToMilliseconds(int progress, long totalDuration) {
+        return (int) ((((double) progress) / 100) * totalDuration);
     }
 }
