83cbebcfd916f6bb7a103661a540d6b761977437
William Wong <williamwong09@gmail.com>
2015-12-12 10:04:48 -0500

    Add Material share icon

--- app/src/main/java/org/williamwong/spotifystreamer/activities/PlayerActivity.java
+++ app/src/main/java/org/williamwong/spotifystreamer/activities/PlayerActivity.java
@@ -1,17 +1,12 @@
 package org.williamwong.spotifystreamer.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import org.williamwong.spotifystreamer.R;
 import org.williamwong.spotifystreamer.fragments.PlayerFragment;
-import org.williamwong.spotifystreamer.models.TrackModel;
-import org.williamwong.spotifystreamer.services.MusicService;
 
 public class PlayerActivity extends AppCompatActivity {
 
@@ -38,30 +33,5 @@ public class PlayerActivity extends AppCompatActivity {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.playerContainer, mPlayerFragment, MainActivity.PLAYER_FRAGMENT_DIALOG_TAG);
         ft.commit();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_share, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_item_share:
-                TrackModel track = MusicService.getMusicService().getCurrentlyPlayingTrackModel();
-                if (track != null) {
-                    Intent shareIntent = new Intent();
-                    shareIntent.setAction(Intent.ACTION_SEND);
-                    shareIntent.setType("text/plain");
-                    shareIntent.putExtra(Intent.EXTRA_TITLE, track.getTrackName());
-                    shareIntent.putExtra(Intent.EXTRA_TEXT, track.getExternalUrl());
-                    startActivity(shareIntent);
-                }
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 }
--- app/src/main/java/org/williamwong/spotifystreamer/fragments/PlayerFragment.java
+++ app/src/main/java/org/williamwong/spotifystreamer/fragments/PlayerFragment.java
@@ -1,16 +1,22 @@
 package org.williamwong.spotifystreamer.fragments;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 
 import org.williamwong.spotifystreamer.R;
 import org.williamwong.spotifystreamer.databinding.FragmentPlayerBinding;
+import org.williamwong.spotifystreamer.models.TrackModel;
+import org.williamwong.spotifystreamer.services.MusicService;
 import org.williamwong.spotifystreamer.viewModels.PlayerViewModel;
 
 /**
@@ -22,6 +28,7 @@ public class PlayerFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_player, container, false);
+        setHasOptionsMenu(true);
 
         FragmentPlayerBinding binding = FragmentPlayerBinding.bind(view);
         PlayerViewModel viewModel = new PlayerViewModel();
@@ -47,5 +54,29 @@ public class PlayerFragment extends DialogFragment {
         Dialog dialog = super.onCreateDialog(savedInstanceState);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         return dialog;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_share, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_share:
+                TrackModel track = MusicService.getMusicService().getCurrentlyPlayingTrackModel();
+                if (track != null) {
+                    Intent shareIntent = new Intent();
+                    shareIntent.setAction(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_TITLE, track.getTrackName());
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, track.getExternalUrl());
+                    startActivity(shareIntent);
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
